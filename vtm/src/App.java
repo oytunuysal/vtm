965feec209327d68a7556657b6cc94104e192954
oytunuysal <nurettinoytunuysal@gmail.com>
2021-04-14 02:51:10 +0300

    update

--- vtm/src/App.java
+++ vtm/src/App.java
@@ -5,92 +5,130 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
-        System.out.println("Input: ");
-        String rolls = input.nextLine();
-        int numberOfDice;
-        int maxDice;
+        while (true) {
+            System.out.println("Input (e.g. 5d10): ");
+            String rolls = input.nextLine();
+            int numberOfDice;
+            int maxDice;
 
-        Random randomGenerator = new Random();
+            // Random randomGenerator = new Random();
 
-        int indexD = rolls.lastIndexOf("d");
-        numberOfDice = Integer.parseInt(rolls.substring(0, indexD));
-        maxDice = Integer.parseInt(rolls.substring(indexD + 1));
-        System.out.println("numberOfDice = " + numberOfDice);
-        System.out.println("maxDice = " + maxDice);
+            int indexD = rolls.lastIndexOf("d");
+            numberOfDice = Integer.parseInt(rolls.substring(0, indexD));
+            maxDice = Integer.parseInt(rolls.substring(indexD + 1));
+            System.out.println("Number Of Dice = " + numberOfDice);
+            System.out.println("Max Dice = " + maxDice);
 
-        float[][] results = new float[maxDice][4];
-        int[][] doubleSuccessResults = new int[maxDice][4];
-        int[] randomRolls = new int[numberOfDice];
-        int[] currentSuccess;
-        int currentFail = 0;
-        int currentIndex = 0;
+            float[][] results = new float[maxDice][5];
+            float[][] doubleSuccessResults = new float[maxDice][5];
+            int[] randomRolls = new int[numberOfDice];
+            int[] currentSuccess;
+            int[] currentDoubleSuccess;
+            int currentFail = 0;
+            int currentIndex = 0;
 
-        for (int i = 0; i < 10000; i++) {
-            currentSuccess = new int[maxDice];
-            currentFail = 0;
+            int rounds = 100000;
 
-            // creating random rolls
-            for (int j = 0; j < numberOfDice; j++) {
-                randomRolls[j] = 1 + randomGenerator.nextInt(10);
-            }
-
-            // sorting
-            Arrays.sort(randomRolls);
-
-            // finding total fails
-            for (int j = 0; j < numberOfDice; j++) {
-                if (randomRolls[j] == 1) {
-                    currentFail++;
-                } else {
-                    break;
+            for (int i = 0; i < rounds; i++) {
+                currentSuccess = new int[maxDice];
+                currentDoubleSuccess = new int[maxDice];
+                currentFail = 0;
+                // creating random rolls
+                for (int j = 0; j < numberOfDice; j++) {
+                    // randomRolls[j] = 1 + randomGenerator.nextInt(10);
+                    randomRolls[j] = (int) getRandomIntegerBetweenRange(1, 10);
                 }
-            }
 
-            // finding the success numbers for each turn
-            currentIndex = currentFail;
-            for (int threshold = 2; threshold <= maxDice; threshold++) {
-                for (int index = currentIndex; index < numberOfDice; index++) {
-                    if (threshold <= randomRolls[index]) {
-                        currentSuccess[threshold - 1] = numberOfDice - index; // ??????????
-                        //currentIndex = index + 1;
+                // sorting
+                Arrays.sort(randomRolls);
+
+                // finding total fails
+                for (int j = 0; j < numberOfDice; j++) {
+                    if (randomRolls[j] == 1) {
+                        currentFail++;
+                    } else {
                         break;
                     }
                 }
+
+                // finding the success numbers for each turn
+                currentIndex = currentFail;
+                for (int threshold = 2; threshold <= maxDice; threshold++) {
+                    for (int index = currentIndex; index < numberOfDice; index++) {
+                        if (threshold <= randomRolls[index]) {
+                            currentSuccess[threshold - 1] = numberOfDice - index; // for non speciality dices
+                            currentDoubleSuccess[threshold - 1] = numberOfDice - index; // for actions with specialities
+                            if (threshold == 10) {
+                                for (int a = 1; a < maxDice; a++) {
+                                    currentDoubleSuccess[a] = currentDoubleSuccess[a] + (numberOfDice - index);
+                                }
+                            }
+                            // currentIndex = index + 1;
+                            break;
+                        }
+                    }
+                }
+
+                statistics(results, currentSuccess, currentFail, maxDice);
+                statistics(doubleSuccessResults, currentDoubleSuccess, currentFail, maxDice);
+
             }
 
-            // botches and other statistics
-            for (int threshold = 1; threshold < maxDice; threshold++) {
-                if ((currentSuccess[threshold] == 0) && (currentFail > 0)) {
-                    // its a botch
-                    results[threshold][3] = results[threshold][3] + 1;
-                } else if (currentSuccess[threshold] < currentFail) {
+            calculateStatistics(results, maxDice, rounds);
+            calculateStatistics(doubleSuccessResults, maxDice, rounds);
+
+            printResults(results);
+            printResults(doubleSuccessResults);
+        }
+    }
+
+    private static double getRandomIntegerBetweenRange(double min, double max) {
+        double x = (int) (Math.random() * ((max - min) + 1)) + min;
+        return x;
+    }
+
+    static private void statistics(float[][] results, int[] currentSuccess, int currentFail, int maxDice) {
+        // botches and other statistics
+        for (int threshold = 1; threshold < maxDice; threshold++) {
+            if ((currentSuccess[threshold] == 0) && (currentFail > 0)) {
+                // botch
+                results[threshold][4] = results[threshold][4] + 1;
+            } else if (currentSuccess[threshold] <= currentFail) {
+                // ingame-fail move
+                results[threshold][2] = results[threshold][2] + 1;
+                if(currentSuccess[threshold] < currentFail){
                     // fail move
-                    results[threshold][2] = results[threshold][2] + 1;
-                } else if (currentSuccess[threshold] > currentFail) {
-                    // success!
-                    results[threshold][0] = results[threshold][0] + (currentSuccess[threshold] - currentFail);
-                    results[threshold][1] = results[threshold][1] + 1;
+                    results[threshold][3] = results[threshold][3] + 1;
                 }
+            } else if (currentSuccess[threshold] > currentFail) {
+                // success!
+                results[threshold][0] = results[threshold][0] + (currentSuccess[threshold] - currentFail);
+                results[threshold][1] = results[threshold][1] + 1;
             }
         }
+    }
 
+    static private void calculateStatistics(float[][] results, int maxDice, int rounds) {
         // final statistics
         for (int threshold = 1; threshold < maxDice; threshold++) {
             // average success
-            results[threshold][0] = results[threshold][0] / 10000;
+            results[threshold][0] = results[threshold][0] / rounds;
             // success rate
-            results[threshold][1] = (results[threshold][1] / 10000) * 100;
+            results[threshold][1] = (results[threshold][1] / rounds) * 100;
+            // ingame-fail rate
+            results[threshold][2] = (results[threshold][2] / rounds) * 100;
             // fail rate
-            results[threshold][2] = (results[threshold][2] / 10000) * 100;
+            results[threshold][3] = (results[threshold][3] / rounds) * 100;
             // botch rate
-            results[threshold][3] = (results[threshold][3] / 10000) * 100;
+            results[threshold][4] = (results[threshold][4] / rounds) * 100;
         }
+    }
 
-
-        //prints
-        System.out.println("Threshold\tSuccess\t\tSuccessRate\tFaiRate\t\tBotchRate");
+    static private void printResults(float[][] results) {
+        // prints
+        System.out.println("Threshold\tSuccess\t\tSuccessRate\tFailRate\tNegativeRate\tBotchRate");
         for (int i = 1; i < results.length; i++) { // this equals to the row in our matrix.
-            System.out.print((i+1) + " \t\t");
+            System.out.print((i + 1) + " \t\t");
             for (int j = 0; j < results[i].length; j++) { // this equals to the column in each row.
                 System.out.printf("%.2f", results[i][j]);
                 System.out.print("\t\t");
